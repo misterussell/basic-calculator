51d85c85ccbf6ac6fca27cb5f1e1a7cf04e96f60
Maxwell Russell <maxruss.87@gmail.com>
2016-10-07 10:17:33 -0500

    click function added yay

--- main.js
+++ main.js
@@ -8,7 +8,26 @@ function add() {
   console.log(delta);
 }
 
+document.querySelector('.click').onclick = function () {
+  console.log('test');
+  var x = Number(document.querySelector('.valOne').value);
+  var y = Number(document.querySelector('.valTwo').value);
+  // var y = 2;
+  var delta = (x + y);
+  console.log(delta);
+ 
+};
+
 // when querySelector('onclick') onclick
-function onclick() {
-  
-}
+// function onclick() {
+//   // var x = document.querySelector('.click').onclick;
+//   console.log(x);
+//   if (x !== null) {
+//     console.log("Test");
+//   }
+//   else {
+//     console.log("Nope. Didn't work.");
+//   }
+// }
+
+// onclick();
